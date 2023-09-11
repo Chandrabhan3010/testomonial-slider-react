4104734c89e4387d7a821e0b579460bd306b9fd5
Chandrabhan Rahangdale <nikhilrahangdale786@gmail.com>
2023-09-12 01:12:17 +0530

    component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <h1>Our Testimonials</h1>
+        <div></div>
+        <Testimonials/>
+      </div>
+    </div>
+  );
 };
 
 export default App;
